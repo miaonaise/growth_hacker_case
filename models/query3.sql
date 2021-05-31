71e7b05b0689b3edce40c6f59f9328da703c082f
julia970906@gmail.com <julia970906@gmail.com>
2021-05-31 17:37:25 +0000

    updated names of variables

--- models/query3.sql
+++ models/query3.sql
@@ -7,7 +7,8 @@ WITH newyork AS(
     WHERE birth_year IS NOT NULL AND gender IS NOT NULL
 )
 
-SELECT COUNT(*) AS counts,gender,age FROM newyork
+SELECT COUNT(*) AS Riders,gender AS Gender,age AS Age
+FROM newyork
 WHERE date <= DATE("2017-05-31") AND date >= DATE("2017-05-01")
 GROUP BY gender,age
 ORDER BY age
